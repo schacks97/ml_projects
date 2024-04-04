97e982fe64f542d7a901aea183321e2828abf4d8
schacks97 <sankhadeep31@gmail.com>
2024-04-04 15:00:01 +0530

    flask implement

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,14 +16,11 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-
 from src.utils import save_object, evaluate_models
-
 
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
-
 
 class ModelTrainer:
     def __init__(self):
@@ -31,7 +28,6 @@ class ModelTrainer:
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
-
             logging.info("Split training and test input data")
             X_train, y_train, X_test, y_test = (
                 train_array[:, :-1],
@@ -39,7 +35,6 @@ class ModelTrainer:
                 test_array[:, :-1],
                 test_array[:, -1]
             )
-
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -47,12 +42,12 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
 
             params = {
-                "Decision Tree": {
-                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                "Decision Tree":{
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
                 },
@@ -85,16 +80,12 @@ class ModelTrainer:
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 }
-
             }
 
             model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
                                                  models=models, param=params)
-
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
-
-            ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
@@ -106,8 +97,6 @@ class ModelTrainer:
 
             logging.info(f"Best found model on both training and testing dataset is {best_model_name}")
 
-
-            ##save the best model in pickle formet
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
